018c6191c441c7391c3e85faf9e1f63e7d7fa5fa
Akyuna Akish <akyunaakish@gmail.com>
2017-04-25 18:19:55 -0700

    added hexadecimal

---
+++ 02_numeral_systems/03_hexadecimal/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("%d - %b - %x \n", 42, 42, 42)
+	//# adds leading 0 for octal and leading X for hex
+	fmt.Printf("%d - %b - %#x \n", 42, 42, 42)
+	fmt.Printf("%d - %b - %#X \n", 42, 42, 42)
+	fmt.Printf("%d \t %b \t %#X \n", 42, 42, 42)
+}
